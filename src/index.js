e73d40c544ec2a8e16268679c1276620ff51dd45
cbuck5 <chris.buckley@nike.com>
2018-11-22 12:25:34 -0800

    cleaned the app

--- src/index.js
+++ src/index.js
@@ -3,29 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
-
-const client = new ApolloClient({
-    uri: "http://localhost:8080/graphql"
-});
-
-client.query({
-    query: gql`
-        {
-            city(zipCode: "97206"){
-                id
-                zipCode
-                name
-                temp
-                timeZone
-                elevation
-                lat
-                lon
-            }
-        }
-    `
-}).then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
